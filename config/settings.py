a1cd282d1efb3ca423961b0a7872c378f60bdd97
Skibitoilet07 <capacidad777@gmail.com>
2025-12-09 02:12:08 -0300

    Switch database backend from MySQL to PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -93,17 +93,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # BASE DE DATOS (MariaDB / MySQL)
 # -----------------------------------------------------------
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",  # MariaDB usa el backend MySQL
-        "NAME": os.getenv("DB_NAME", "sequia_db"),
-        "USER": os.getenv("DB_USER", "sequia_user"),
-        "PASSWORD": os.getenv("DB_PASS", "Tu_Clave_Segura_2025"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "3306"),
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-            "charset": "utf8mb4",
-        },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
