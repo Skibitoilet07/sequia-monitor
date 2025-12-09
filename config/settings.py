27b0d774edcd9c794575e0d6bc873a0e656b770e
Skibitoilet07 <capacidad777@gmail.com>
2025-12-09 02:18:09 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -7,6 +7,7 @@ from pathlib import Path
 import os
 from datetime import timedelta
 from dotenv import load_dotenv
+import dj_database_url
 
 # -----------------------------------------------------------
 # RUTAS BASE
@@ -93,14 +94,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 # BASE DE DATOS (MariaDB / MySQL)
 # -----------------------------------------------------------
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 # -----------------------------------------------------------
